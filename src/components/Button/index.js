13f9ae09420a1aa1367e54d999c331bcd814192d
nhatban25912345 <hoangnhat529@gmail.com>
2022-11-02 23:40:27 +0700

    xay dung ui  #3

---
+++ src/components/Button/index.js
@@ -0,0 +1,60 @@
+import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
+import styles from "./Button.module.scss";
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to, href, onClick, 
+    primary = false, 
+    outline = false, 
+    blackborder = false, 
+    disabled = false,
+    rounded = false, 
+    leftIcon = false,
+    rightIcon = false,
+    children,  
+    ...passProps}) {
+    
+    let Comp = "button";
+    const classes = cx("wrapper", {
+        primary,
+        outline,
+        blackborder,
+        disabled,
+        rounded,
+        leftIcon,
+        rightIcon
+    });
+    
+    const props = {
+        onClick,
+        ...passProps,
+    }
+
+    if (disabled) {
+        Object.keys(props).forEach(key => {
+            if (key.startsWith("on") && typeof props[key] === "function") {
+                delete props[key];  
+            }
+        });
+    }
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = Link;
+    }
+
+    return ( 
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx("icon")}>{leftIcon}</span >}
+            <span className={cx("title-button")}>{children}</span>
+            {rightIcon && <span className={cx("icon")}>{rightIcon}</span >}
+        </Comp>
+    );
+}
+
+export default Button;
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -2,12 +2,13 @@ import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Tippy from '@tippyjs/react/headless';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faMagnifyingGlass, faPlus, faSignIn, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 import { Wrapper as PropperWrapper } from '~/components/Propper'
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import AccountItem from '~/components/AccountItem';
+import Button from '~/components/Button';
 
 const cx = classNames.bind(styles);
 
@@ -25,7 +26,9 @@ function Header() {
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <div className={cx('logo')}>
-                    <img src={images.logo} alt="Tiktok" />
+                    <a href="/" className={cx("logo-container")}>
+                        <img src={images.logo} alt="Tiktok"/>
+                    </a>
                 </div>
                 <div className={cx('search')}>
                     <Tippy
@@ -54,7 +57,14 @@ function Header() {
                         <FontAwesomeIcon icon={faMagnifyingGlass} />
                     </button>
                 </div>
-                <div className={cx('action')}></div>
+                <div className={cx('actions')}>
+                    <Button blackborder>
+                        <FontAwesomeIcon className={cx("plus-icon")} icon={faPlus}/>
+                        <span>Upload</span>
+                    </Button>
+                    <Button primary rightIcon={<FontAwesomeIcon className={cx("plus-icon")} icon={faSignIn}/>}>Log in</Button>
+                    <i className={cx("option-btn")}><svg width="20px" height="20px" viewBox="0 0 48 48" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M24 4C26.2091 4 28 5.79086 28 8C28 10.2091 26.2091 12 24 12C21.7909 12 20 10.2091 20 8C20 5.79086 21.7909 4 24 4ZM24 20C26.2091 20 28 21.7909 28 24C28 26.2091 26.2091 28 24 28C21.7909 28 20 26.2091 20 24C20 21.7909 21.7909 20 24 20ZM24 36C26.2091 36 28 37.7909 28 40C28 42.2091 26.2091 44 24 44C21.7909 44 20 42.2091 20 40C20 37.7909 21.7909 36 24 36Z"></path></svg></i>
+                </div>
             </div>
         </header>
     );
